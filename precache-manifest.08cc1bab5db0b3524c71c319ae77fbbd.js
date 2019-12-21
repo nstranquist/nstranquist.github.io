5e057d1b184c9934fb306e671c77470b947659b7
Nico Stranquist <nstranquist@gmail.com>
2019-12-21 15:54:43 -0600

    Updates

---
+++ precache-manifest.08cc1bab5db0b3524c71c319ae77fbbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32795287acac975a5d036bb4378dcd64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1df0c4f9d374a441fc0c",
+    "url": "/static/js/2.d8918cc5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.d8918cc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8ece7c8e0752b638724",
+    "url": "/static/js/main.c82616b9.chunk.js"
+  },
+  {
+    "revision": "64990688fee478bf5962",
+    "url": "/static/js/runtime-main.2592e00e.js"
+  }
+]);
